b438ff60cc916a3d01ab0ad5944d2c89caad1971
Davina Leong <leong.shi.yun@gmail.com>
2023-08-11 10:04:42 +0800

    Committing existing files

---
+++ src/scripts/generator.js
@@ -0,0 +1,13 @@
+console.log(`generator.js loaded`)
+
+const dataElementAttr = `data-element`
+
+const outputEl = document.querySelector(`[${dataElementAttr}="output"]`)
+
+if (outputEl) {
+  let html = ``
+
+  // Load styles
+
+  outputEl.innerHTML = html
+}
