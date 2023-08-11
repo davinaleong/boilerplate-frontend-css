8795a2e20c7f04a8ea0ae87b6c962fdacc15da48
Davina Leong <leong.shi.yun@gmail.com>
2023-08-11 11:27:35 +0800

    WIP code generator

--- src/scripts/styles.js
+++ src/scripts/styles.js
@@ -1,1 +1,91 @@
 console.log(`styles.js loaded`)
+
+const styles = {
+  colours: [
+    {
+      key: "primary",
+      min: 0,
+      max: 900,
+    },
+    {
+      key: "secondary",
+      min: 0,
+      max: 900,
+    },
+    {
+      key: "accent-1",
+      min: 0,
+      max: 900,
+    },
+    {
+      key: "gray",
+      min: 0,
+      max: 1000,
+    },
+  ],
+  fontFamilies: [
+    {
+      key: "primary",
+      value: "primary",
+    },
+    {
+      key: "secondary",
+      value: "secondary",
+    },
+    {
+      key: "monospace",
+      value: "monospace",
+    },
+  ],
+  fontSizes: { min: 0, max: 900 },
+  fontWeights: [
+    {
+      key: "thin",
+      value: "thin",
+    },
+    {
+      key: "extra-light",
+      value: "extra-light",
+    },
+    {
+      key: "light",
+      value: "light",
+    },
+    {
+      key: "regular",
+      value: "regular",
+    },
+    {
+      key: "medium",
+      value: "medium",
+    },
+    {
+      key: "semi-bold",
+      value: "semi-bold",
+    },
+    {
+      key: "bold",
+      value: "bold",
+    },
+    {
+      key: "extra-bold",
+      value: "extra-bold",
+    },
+    {
+      key: "black",
+      value: "black",
+    },
+  ],
+  sizes: [
+    {
+      key: "f",
+      min: 0,
+      max: 1000,
+    },
+    {
+      key: "v",
+      min: 0,
+      max: 1000,
+    },
+  ],
+}
